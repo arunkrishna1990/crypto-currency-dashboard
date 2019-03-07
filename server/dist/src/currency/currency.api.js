04e146279659412bc29407fe51786d8e97f59e2f
arun@fantoo.net <arun@fantoo.net>
2019-03-07 11:11:12 +0000

    Removing the dist folder committed accidentally

--- server/dist/src/currency/currency.api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const currency_controller_1 = require("./currency.controller");
-exports.routes = express_1.Router();
-exports.routes.get('/', currency_controller_1.CurrencyController.getLatestCryptoListings);
--- server/dist/src/shared/HttpStatusCodes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HttpStatusCodes {
-}
-HttpStatusCodes.OK = 200;
-HttpStatusCodes.BadRequest = 400;
-HttpStatusCodes.InternalServerError = 500;
-exports.HttpStatusCodes = HttpStatusCodes;
